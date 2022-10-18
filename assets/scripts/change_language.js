5dd0169c6b999823f7d56652fa96c21c21765220
Alexander Minkin <weryskok@gmail.com>
2022-10-18 23:16:29 +0300

    Auto-detect language

---
+++ assets/scripts/change_language.js
@@ -0,0 +1,41 @@
+function usePrefferedLanguage(newLang) {
+    function changeLanguageTo(language) {
+        if (document.title.includes('404')) return; // TODO: make an english version of 404
+        if (document.querySelector('html').lang == language) return;
+        switch (language) {
+            case 'en':
+                location.pathname = `/en${location.pathname}`;
+                break;
+
+            case 'ru':
+                location.pathname = location.pathname.replace('/en', '');
+                break;
+
+            default:
+                break;
+        }
+    }
+
+    function browserPrefferedLanguage() {
+        if (navigator.languages.includes('ru')) {
+            return 'ru';
+        }
+        return 'en';
+    }
+
+    let prefferedLanguage = localStorage.getItem('prefferedLanguage');
+
+    if (newLang) {
+        localStorage.setItem('prefferedLanguage', newLang);
+        prefferedLanguage = localStorage.getItem('prefferedLanguage');
+    }
+
+    if (!prefferedLanguage) {
+        localStorage.setItem('prefferedLanguage', browserPrefferedLanguage());
+        prefferedLanguage = localStorage.getItem('prefferedLanguage');
+    }
+
+    changeLanguageTo(prefferedLanguage);
+}
+
+usePrefferedLanguage();
